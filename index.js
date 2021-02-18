604503ff59aca443f2098c2459687324c288cf34
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-02-18 20:29:34 +1100

    [{package,package-lock}.json] Upgrade dependencies; bump version; [tsconfig.json] Remove comments; [index.ts] Conform to latest version of isomorphic-git and new TypeScript strength; [index.js] Recompiled

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.acquireGithubWiki = void 0;
 const fs = __importStar(require("fs"));
 const os_1 = require("os");
-const git = __importStar(require("isomorphic-git"));
 const path = __importStar(require("path"));
-git.plugins.set('fs', fs);
-exports.acquireGithubWiki = (url, to_dir, callback) => {
-    const dir = to_dir || path.join(os_1.tmpdir(), url.slice(url.lastIndexOf('/') + 1));
-    git
-        .clone({
-        dir, url,
+const isomorphic_git_1 = require("isomorphic-git");
+const http = __importStar(require("isomorphic-git/http/node"));
+const acquireGithubWiki = (url, to_dir, callback) => {
+    const dir = to_dir == null ? path.join(os_1.tmpdir(), url.slice(url.lastIndexOf('/') + 1)) : to_dir;
+    isomorphic_git_1.clone({
+        fs,
+        http,
+        url,
+        dir,
         corsProxy: 'https://cors.isomorphic-git.org',
         ref: 'master',
         singleBranch: true,
-        depth: 10
-    })
-        .then(() => git
-        .listFiles({ fs, dir })
+        depth: 10,
+    }).then(() => isomorphic_git_1.listFiles({ fs, dir })
         .then((files) => callback(void 0, new Map(files
         .filter((fname) => fname.endsWith('.md') && !fname.startsWith(`.github${path.sep}`))
         .map((fname) => [
@@ -32,6 +45,7 @@ exports.acquireGithubWiki = (url, to_dir, callback) => {
         .catch(callback))
         .catch(callback);
 };
+exports.acquireGithubWiki = acquireGithubWiki;
 if (require.main === module) {
     exports.acquireGithubWiki('https://github.com/isomorphic-git/isomorphic-git', void 0, (err, fname2html) => {
         if (err != null)
